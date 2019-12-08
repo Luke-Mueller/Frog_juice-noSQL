4fcae3e5166f5c957e50f1ac18d79bb70ca5e8e7
Luke Mueller <muelll79@hotmail.com>
2019-12-08 14:00:19 -0700

    installs express-session and stores session and creates get and post login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
 
 const errorController = require('./controllers/404');
 const User = require('./models/user');
@@ -18,6 +19,7 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'longstringvalue', resave: false, saveUninitialized: false}));
 
 app.use((req, res, next) => {
   User.findById('5dec1f0beb064d3a9b290527')
