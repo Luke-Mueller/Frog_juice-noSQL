4844b51e2d857809ebb06bf466b4e2b73e61cfa5
Luke Mueller <muelll79@hotmail.com>
2019-12-16 13:58:37 -0700

    adds signup button to nav and user signup to MongoDB

--- controllers/auth.js
+++ controllers/auth.js
@@ -9,7 +9,7 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  User.findById('5dec1f0beb064d3a9b290527')
+  User.findById('5df6d6d76b4f65f949f6b7a8')
     .then(user => {
       req.session.isLoggedIn = true;
       req.session.user = user;
@@ -18,7 +18,41 @@ exports.postLogin = (req, res, next) => {
         res.redirect('/');
       });
     })
-    .catch(err => console.log(err));
+    .catch(
+      err => console.log(err)
+    );
+};
+
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: true
+  })
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        return res.redirect('/signup');
+      } 
+      const user = new User({
+        email: email,
+        password: password,
+        cart: { items: [] }
+      })
+      return user.save();
+    })
+    .then(result => {
+      res.redirect('/login');
+    })
+    .catch(err => {
+      err => console.log(err)
+    });
 };
 
 exports.postLogout = (req, res, next) => {
