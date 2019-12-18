bd17f41f5f4c2e33cc577846c7c4b9fac33e8177
Luke Mueller <muelll79@hotmail.com>
2019-12-17 23:22:01 -0700

    adds new password view and functionality

--- routes/auth.js
+++ routes/auth.js
@@ -13,6 +13,9 @@ router.post('/login', authController.postLogin);
 router.get('/reset', authController.getReset);
 router.post('/reset', authController.postReset);
 
+router.get('/reset/:token', authController.getNewPassword);
+router.post('/new-password', authController.postNewPassword);
+
 router.post('/logout', authController.postLogout);
 
 module.exports = router;
