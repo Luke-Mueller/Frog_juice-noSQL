db619c87a3be77cceb0fa2c78b6ae06f34c91ea0
Luke Mueller <muelll79@hotmail.com>
2020-01-19 20:07:35 -0700

    adds data to invoice

--- controllers/shop.js
+++ controllers/shop.js
@@ -166,7 +166,22 @@ exports.getInvoice = (req, res, next) => {
       pdfDoc.pipe(fs.createWriteStream(invoicePath));
       pdfDoc.pipe(res);
 
-      pdfDoc.text('Hello World');
+      pdfDoc.fontSize(26).text('Invoice', {
+        underline: true
+      });
+
+      pdfDoc.text('-----------------------------------');
+
+      let totalPrice = 0;
+      order.products.forEach(prod => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc
+          .fontSize(16)
+          .text(prod.product.title + ' - ' + prod.quantity + ' x $' + prod.product.price);
+      });
+
+      pdfDoc.text('Total Price: $' + totalPrice);
+
       pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
